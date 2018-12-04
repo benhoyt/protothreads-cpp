984aa540dd4325b7e23dc76135ca28a36526f0c6
Ben Hoyt <benhoyt@gmail.com>
2018-12-04 16:48:52 -0500

    Add explicit LICENSE.txt file. Fixes #2

--- Protothread.h
+++ Protothread.h
@@ -15,36 +15,9 @@
 // C/C++, General, Debug Information Format, and change it to "Program
 // Database (/Zi)".
 //
-// --------------------------
-// Original BSD-style license
-// --------------------------
-// Copyright (c) 2004-2005, Swedish Institute of Computer Science.
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-//
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-// 3. Neither the name of the Institute nor the names of its contributors
-//    may be used to endorse or promote products derived from this software
-//    without specific prior written permission.
-//
-// This software is provided by the Institute and contributors "as is" and
-// any express or implied warranties, including, but not limited to, the
-// implied warranties of merchantability and fitness for a particular purpose
-// are disclaimed. In no event shall the Institute or contributors be liable
-// for any direct, indirect, incidental, special, exemplary, or consequential
-// damages (including, but not limited to, procurement of substitute goods
-// or services; loss of use, data, or profits; or business interruption)
-// however caused and on any theory of liability, whether in contract, strict
-// liability, or tort (including negligence or otherwise) arising in any way
-// out of the use of this software, even if advised of the possibility of
-// such damage.
+// Protothread.h is licensed under an MIT license (see LICENSE.txt).
+// Adam's original library was licensed under a similar, BSD-style
+// license, which lives at: http://dunkels.com/adam/pt/license.html
 //
 
 #ifndef __PROTOTHREAD_H__
